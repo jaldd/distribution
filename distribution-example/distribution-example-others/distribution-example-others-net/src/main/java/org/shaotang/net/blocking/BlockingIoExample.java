4bf01925498079c9478e0e8d958cee4928ac1e09
李丹丹(10037750) <lidd-h@glodon.com>
2021-11-12 17:07:56 +0800

    BlockingIo

---
+++ distribution-example/distribution-example-others/distribution-example-others-net/src/main/java/org/shaotang/net/blocking/BlockingIoExample.java
@@ -0,0 +1,32 @@
+package org.shaotang.net.blocking;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class BlockingIoExample {
+
+    public void serve(int port) throws IOException {
+
+        ServerSocket serverSocket = new ServerSocket(port);
+        Socket clientSocket = serverSocket.accept();
+        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), false);
+        String request, response;
+        while ((request = in.readLine()) != null) {
+            if ("Done".equals(request)) {
+                break;
+            }
+            response = processRequest(request);
+            out.println(response);
+        }
+    }
+
+    private String processRequest(String request) {
+
+        return "Processed";
+    }
+}
