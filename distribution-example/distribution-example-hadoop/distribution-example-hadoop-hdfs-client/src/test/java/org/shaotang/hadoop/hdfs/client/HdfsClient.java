fec2aca2cb04e9c17c3e800ff3c34eafbfecb802
lidd-h <lidd-h@glodon.com>
2021-12-14 20:46:29 +0800

    上传下载

--- distribution-example/distribution-example-hadoop/distribution-example-hadoop-hdfs-client/src/test/java/org/shaotang/hadoop/hdfs/client/HdfsClient.java
+++ distribution-example/distribution-example-hadoop/distribution-example-hadoop-hdfs-client/src/test/java/org/shaotang/hadoop/hdfs/client/HdfsClient.java
@@ -2,8 +2,7 @@ package org.shaotang.hadoop.hdfs.client;
 
 import lombok.extern.slf4j.Slf4j;
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.FileSystem;
-import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.*;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -11,6 +10,7 @@ import org.junit.Test;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.Arrays;
 
 @Slf4j
 public class HdfsClient {
@@ -19,10 +19,11 @@ public class HdfsClient {
 
     @Before
     public void init() throws IOException, InterruptedException, URISyntaxException {
-        URI uri=new URI("hdfs://hadoop-pseudo:8020");
+        URI uri = new URI("hdfs://hadoop-pseudo:8020");
 
-        Configuration config=new Configuration();
-        fs = FileSystem.get(uri, config,"root");
+        Configuration config = new Configuration();
+        config.set("dfs.replication", "2");
+        fs = FileSystem.get(uri, config, "root");
 
     }
 
@@ -30,16 +31,56 @@ public class HdfsClient {
     public void close() throws IOException {
         fs.close();
     }
-    
+
     @Test
     public void testMkdir() throws URISyntaxException, IOException, InterruptedException {
 
 
-        fs.mkdirs(new Path("/xiyou/tianting"));
+        fs.mkdirs(new Path("/xiyou/huaguoshan"));
 
 
     }
 
+    @Test
+    public void testPutFile() throws IOException {
+        fs.copyFromLocalFile(false, true,
+                new Path("/usr/local/hadoop/hadoop-3.1.4/test/wukong.txt"),
+                new Path("/xiyou/huaguoshan"));
+    }
+
+    @Test
+    public void testGetFile() throws IOException {
+        fs.copyToLocalFile(false, new Path("/xiyou/huaguoshan/wukong.txt")
+                , new Path("/usr/local/hadoop/hadoop-3.1.4/test/sunwukong2.txt"), false);
+    }
+
+    @Test
+    public void testRm() throws IOException {
+        fs.delete(new Path("/xiyou/huaguoshan"), true);
+    }
+
+    @Test
+    public void testMv() throws IOException {
+        fs.rename(new Path("/xiyou/huaguoshan/wukong.txt"), new Path("/xiyou/huaguoshan/liuer.txt"));
+    }
+
+    @Test
+    public void testFileDetail() throws IOException {
+        RemoteIterator<LocatedFileStatus> listFiles = fs.listFiles(new Path("/"), true);
+        while (listFiles.hasNext()) {
+            LocatedFileStatus fileStatus = listFiles.next();
+            System.out.println(fileStatus.getPath());
+            System.out.println(fileStatus.getPath().getName());
+            System.out.println(fileStatus.isFile());
+            BlockLocation[] locations = fileStatus.getBlockLocations();
+            System.out.println(Arrays.asList(locations));
+        }
+
+        FileStatus[] fileStatuses = fs.listStatus(new Path("/xiyou/huaguoshan"));
+        for (FileStatus fileStatus : fileStatuses) {
+            System.out.println(fileStatus.getPath().getName() + ":" + fileStatus.isFile());
+        }
+    }
 
     public static void main(String[] args) {
         System.out.println(1);
