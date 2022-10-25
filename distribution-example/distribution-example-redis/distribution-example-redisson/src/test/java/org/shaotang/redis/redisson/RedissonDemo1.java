f8980ea1f666de667b0e416c92d00cd32acc12ed
jaldd <806616508@qq.com>
2022-10-25 17:47:37 +0800

    lock

--- distribution-example/distribution-example-redis/distribution-example-redisson/src/test/java/org/shaotang/redis/redisson/RedissonDemo1.java
+++ distribution-example/distribution-example-redis/distribution-example-redisson/src/test/java/org/shaotang/redis/redisson/RedissonDemo1.java
@@ -1,10 +1,12 @@
 package org.shaotang.redis.redisson;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.redisson.Redisson;
 import org.redisson.api.RAtomicLong;
+import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,7 +14,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
 
+@Slf4j
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = TestApplication.class)
 public class RedissonDemo1 {
@@ -26,6 +30,7 @@ public class RedissonDemo1 {
 //        config.setTransportMode(TransportMode.EPOLL);
         config.useSingleServer().setAddress("redis://hadoop-pseudo:16379");
     }
+
     @Test
     public void test1() throws ExecutionException, InterruptedException {
         RedissonClient client = Redisson.create(config);
@@ -35,7 +40,7 @@ public class RedissonDemo1 {
 // 异步执行方式
         Boolean result = longObject.compareAndSet(3, 401);
         System.out.println(result);
-        }
+    }
 
     @Test
     public void test() throws ExecutionException, InterruptedException {
@@ -58,6 +63,25 @@ public class RedissonDemo1 {
 //        RAtomicLongRx longObject = client.getAtomicLong("myLong");
 //// RxJava2方式
 //        Flowable<Boolean result = longObject.compareAndSet(3, 401);
+    }
+
+    @Test
+    public void testLock() throws InterruptedException {
+        config.setLockWatchdogTimeout(30000);
+        RedissonClient client = Redisson.create(config);
+        RLock lock = client.getLock("anyLock");
+//        lock.lock(10, TimeUnit.SECONDS);
+
+// 尝试加锁，最多等待100秒，上锁以后10秒自动解锁
+        boolean res = lock.tryLock(100, 10, TimeUnit.SECONDS);
+        log.info("res:" + res);
+        if (res) {
+            try {
+                Thread.sleep(21000);
+            } finally {
+                lock.unlock();
+            }
+        }
     }
 
     public static void main(String[] args) {
