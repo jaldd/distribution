68e96c614e2655d23b38266b476f7a454fe0fdaa
jaldd <806616508@qq.com>
2023-01-03 19:47:47 +0800

    有界流 from 集合

--- distribution-example/distribution-example-flink/distribution-example-flink-java/src/main/java/org/shaotang/flink/wc/suanzi/SourceTest.java
+++ distribution-example/distribution-example-flink/distribution-example-flink-java/src/main/java/org/shaotang/flink/wc/suanzi/SourceTest.java
@@ -3,6 +3,9 @@ package org.shaotang.flink.wc.suanzi;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SourceTest {
 
     public static void main(String[] args) throws Exception {
@@ -11,6 +14,11 @@ public class SourceTest {
         DataStreamSource<String> streamSource = environment.readTextFile("distribution-example/distribution-example-flink/distribution-example-flink-java/src/main/resources/input/inputsz/clicks.txt");
         streamSource.print("1");
 
+
+        List<Event> events = new ArrayList<>();
+        events.add(new Event("a", "/a", 100));
+        DataStreamSource<Event> fromCollection = environment.fromCollection(events);
+        fromCollection.print("2");
         environment.execute();
     }
 }
