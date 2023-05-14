c6eee977a26ddbbc310c3283c822524234071d7c
jaldd <806616508@qq.com>
2023-05-14 20:41:06 +0800

    flatmap test

--- distribution-example/distribution-example-flink/distribution-example-flink-java/src/main/java/org/shaotang/flink/wc/suanzi/TransformFlatMapTest.java
+++ distribution-example/distribution-example-flink/distribution-example-flink-java/src/main/java/org/shaotang/flink/wc/suanzi/TransformFlatMapTest.java
@@ -8,4 +8,34 @@ import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.util.Collector;
 
 public class TransformFlatMapTest {
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment environment = StreamExecutionEnvironment.getExecutionEnvironment();
+        environment.setParallelism(1);
+
+        DataStreamSource<Event> streamSource = environment.fromElements(
+                new Event("a", "/a", 200),
+                new Event("b", "/b", 100));
+        SingleOutputStreamOperator<String> map = streamSource.flatMap(new MyFlatMapper());
+
+        map.print("1");
+
+        streamSource.flatMap((FlatMapFunction<Event, String>) (event, collector) -> {
+            collector.collect(event.getUrl());
+            collector.collect(event.getUser());
+            collector.collect(String.valueOf(event.getTimestamp()));
+        }).returns(new TypeHint<String>(){}).print("2");
+
+
+        environment.execute();
+    }
+
+    public static class MyFlatMapper implements FlatMapFunction<Event, String> {
+
+        @Override
+        public void flatMap(Event event, Collector<String> collector) {
+            collector.collect(event.getUrl());
+            collector.collect(event.getUser());
+            collector.collect(String.valueOf(event.getTimestamp()));
+        }
+    }
 }
