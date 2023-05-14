315f3f6e4701267700d4ece639997c5e17f4e934
jaldd <806616508@qq.com>
2023-05-14 20:39:24 +0800

    test

---
+++ distribution-example/distribution-example-flink/distribution-example-flink-java/src/main/java/org/shaotang/flink/wc/suanzi/TransformFlatMapTest.java
@@ -0,0 +1,11 @@
+package org.shaotang.flink.wc.suanzi;
+
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.common.typeinfo.TypeHint;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.util.Collector;
+
+public class TransformFlatMapTest {
+}
