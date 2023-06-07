4077841b67eec138b672a4d9dd33e2c47103365f
jaldd <806616508@qq.com>
2023-06-07 10:50:46 +0800

    jdbc访问starrocks

---
+++ distribution-example/distribution-example-starrocks/distribution-example-starrocks-jdbc/src/main/java/org/shaotang/distribution/example/starrocks/StarrocksJdbcApplication.java
@@ -0,0 +1,13 @@
+package org.shaotang.distribution.example.starrocks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication()
+public class StarrocksJdbcApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(StarrocksJdbcApplication.class, args);
+    }
+}
---
+++ distribution-example/distribution-example-starrocks/distribution-example-starrocks-jdbc/src/test/java/org/shaotang/distribution/example/starrocks/client/Test.java
@@ -0,0 +1,10 @@
+package org.shaotang.distribution.example.starrocks.client;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Test {
+
+
+
+}
