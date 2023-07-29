6c40d15dd8236af1eac67aed0b7c08f6961003f4
jaldd <806616508@qq.com>
2023-07-29 17:57:01 +0800

    自定义分区

--- distribution-example/distribution-example-flink/distribution-example-flink-java/src/main/java/org/shaotang/flink/wc/suanzi/TransformPartitionTest.java
+++ distribution-example/distribution-example-flink/distribution-example-flink-java/src/main/java/org/shaotang/flink/wc/suanzi/TransformPartitionTest.java
@@ -1,8 +1,9 @@
 package org.shaotang.flink.wc.suanzi;
 
+import org.apache.flink.api.common.functions.Partitioner;
+import org.apache.flink.api.java.functions.KeySelector;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.streaming.api.functions.source.RichParallelSourceFunction;
 
 public class TransformPartitionTest {
     public static void main(String[] args) throws Exception {
@@ -26,27 +27,40 @@ public class TransformPartitionTest {
         //轮训
 //        streamSource.rebalance().print().setParallelism(4);
         //rescale 小组内重新分配
-        environment.addSource(new RichParallelSourceFunction<Integer>() {
-            @Override
-            public void run(SourceContext<Integer> sourceContext) throws Exception {
-                for (int i = 0; i < 9; i++) {
-                    if (i % 2 == getRuntimeContext().getIndexOfThisSubtask()) {
-                        sourceContext.collect(i);
-                    }
-                }
-            }
-
-            @Override
-            public void cancel() {
-
-            }
-        }).setParallelism(2)
-//                .rescale().print()
-                .setParallelism(4);
+//        environment.addSource(new RichParallelSourceFunction<Integer>() {
+//                    @Override
+//                    public void run(SourceContext<Integer> sourceContext) throws Exception {
+//                        for (int i = 0; i < 9; i++) {
+//                            if (i % 2 == getRuntimeContext().getIndexOfThisSubtask()) {
+//                                sourceContext.collect(i);
+//                            }
+//                        }
+//                    }
+//
+//                    @Override
+//                    public void cancel() {
+//
+//                    }
+//                }).setParallelism(2)
+////                .rescale().print()
+//                .setParallelism(4);
         //广播
 //        streamSource.broadcast().print().setParallelism(4);
         //全局
-        streamSource.global().print().setParallelism(4);
+//        streamSource.global().print().setParallelism(4);
+        //自定义分区
+        environment.fromElements(1, 2, 3, 4, 5, 6, 7, 8)
+                .partitionCustom(new Partitioner<Integer>() {
+                    @Override
+                    public int partition(Integer key, int i) {
+                        return key % 2;
+                    }
+                }, new KeySelector<Integer, Integer>() {
+                    @Override
+                    public Integer getKey(Integer integer) throws Exception {
+                        return integer;
+                    }
+                }).print().setParallelism(4);
 
         environment.execute();
     }
