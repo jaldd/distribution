e1be738fd397865dfa3115fe782cb61d71ff4079
jaldd <806616508@qq.com>
2023-08-23 17:26:36 +0800

    rabbitmqtestmodule

---
+++ distribution-example/distribution-example-rabbitmq/distribution-example-rabbitmq-client/src/main/java/org/shaotang/distribution/example/rabbitmq/web/ProducerController.java
@@ -0,0 +1,10 @@
+package org.shaotang.distribution.example.rabbitmq.web;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProducerController {
+
+
+
+}
