1d955f1982424132f27278b4fe95487ad3e396f7
jaldd <806616508@qq.com>
2024-01-10 10:43:08 +0800

    test多文件上传测试 & param

--- distribution-example/distribution-example-starrocks/distribution-example-starrocks-load/src/main/java/org/shaotang/distribution/example/starrocks/export/UploadController.java
+++ distribution-example/distribution-example-starrocks/distribution-example-starrocks-load/src/main/java/org/shaotang/distribution/example/starrocks/export/UploadController.java
@@ -9,7 +9,10 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.Part;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class UploadController {
@@ -19,13 +22,17 @@ public class UploadController {
     public String upload(@RequestParam(value = "files", required = false) MultipartFile[] files,
                          HttpServletRequest request) throws ServletException, IOException {
 
+        Map<String, Object> params = new HashMap<>();
+        request.getParameterMap().forEach((k, v) ->
+                params.put(k, Arrays.asList(v))
+        );
 
         for (MultipartFile file : files) {
             System.out.println(file.getOriginalFilename());
         }
         Collection<Part> parts = request.getParts();
         String data = request.getParameter("data");
-        return null;
+        return params.toString();
     }
 
 
